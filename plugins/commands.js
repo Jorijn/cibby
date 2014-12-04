907b840c8f0a0d424173db60e9c4c1218dc55a99
Jorijn Schrijvershof <jorijn@jorijn.com>
2014-12-04 11:48:39 +0100

    added nick info to command

--- plugins/commands.js
+++ plugins/commands.js
@@ -2,22 +2,24 @@ var cfg = require('../config');
 
 module.exports = function() {
     return function(irc) {
-        irc.on('message', function(message) {
-            if (message.to.substr(0, 1) !== '#') {
+        irc.on('data', function(data) {
+            if (data.params.substr(0, 1) !== '#') {
                 return;
             }
 
-            var string = message.message,
-                to = message.to,
+            var string = data.trailing,
+                to = data.params,
                 prefix = cfg.commands.prefix,
                 bits = string.split(' '),
+                from = data.prefix.match(/(.*)!(.*)@(.*)/),
+                from = { nick: from[1], user: from[2], hostname: from[3] },
                 command;
 
             if (string.substr(0, prefix.length) === prefix) {
                 command = bits[0].substr(prefix.length);
                 bits.shift();
 
-                irc.emit('command_' + command, { command: command, channel: to, params: bits, message: message });
+                irc.emit('command_' + command, { command: command, channel: to, params: bits, message: string, from: from });
             }
         });
     }
